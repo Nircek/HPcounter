9730a8ffab0cc970ae79bb18a5d782c94f5475c4
Nircek <nircek-2103@protonmail.com>
2018-12-31 11:12:02 +0100

    Change layout of nav header

--- app/src/main/java/io/github/nircek/hpcounter/MainActivity.kt
+++ app/src/main/java/io/github/nircek/hpcounter/MainActivity.kt
@@ -10,6 +10,7 @@ import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
