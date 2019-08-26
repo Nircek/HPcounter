9a3279d915d6c1db66b0e4f2e109cd03e69093dc
Nircek <nircek-2103@protonmail.com>
2019-08-26 16:58:50 +0200

    Add basic functionality

--- app/src/main/java/io/github/nircek/hpcounter/CounterFragment.kt
+++ app/src/main/java/io/github/nircek/hpcounter/CounterFragment.kt
@@ -5,9 +5,23 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.counter_fragment.*
 
 class CounterFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return LayoutInflater.from(container?.context).inflate(R.layout.counter_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        plus.setOnClickListener {
+            counter.setText((counter.text.toString().toInt()+1).toString())
+        }
+        minus.setOnClickListener {
+            counter.setText((counter.text.toString().toInt()-1).toString())
+        }
+        reset.setOnClickListener {
+            counter.setText("20")
+        }
     }
 }
